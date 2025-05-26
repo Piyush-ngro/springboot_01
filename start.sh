96e32b9cd54b8969d191801bdc18e7533eb4d747
Piyush-ngro <piyush.bhardwaj@nagarro.com>
2025-05-27 01:07:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 echo "Starting sysdig capture in background..."
 sysdig -w /tmp/sysdig_capture.scap &
+disown %1  # Disown the backgrounded Sysdig process
 
 echo "Starting Tomcat server..."
-exec catalina.sh run
+exec catalina.sh run  # Properly handle signals with exec
